45f72107831889b1e9715dd00f620c132651b928
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-15 07:05:47 -0600

    Extract redirect logic into fun

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,24 +1,36 @@
 use axum::{extract::{State, Request}, response::{Redirect, IntoResponse, Response}, middleware::Next};
 pub use axum::middleware::{from_fn_with_state, from_fn};
+use http::StatusCode;
 
 use crate::{state::AppState, auth::Session, error};
 
 pub async fn redirect_already_logged_in(_: State<AppState>, session: Result<Session, error::Error>, req: Request, next: Next) -> Response {
     if session.is_ok() {
-        return Redirect::to("/").into_response();
+        redirect(req, "/")
+    } else {
+        next.run(req).await
     }
-    
-    next.run(req).await
 }
 
 pub async fn assert_is_admin(_: State<AppState>, session: Result<Session, error::Error>, req: Request, next: Next) -> Response {
-    if let Ok(session) = session {
-        if session.is_admin() {
-            return next.run(req).await;
-        }
+    match session {
+        Ok(session) if session.is_admin() => next.run(req).await,
+        _ => redirect(req, "/")
     }
-    
-    Redirect::to("/").into_response()
+}
+
+fn redirect(req: Request, to: &str) -> Response {
+    if req.headers().get("HX-Request").is_some() {
+        let res = StatusCode::OK.into_response();
+
+        let (mut parts, body) = res.into_parts();
+
+        parts.headers.insert("HX-Redirect", to.parse().unwrap());
+
+        Response::from_parts(parts, body)
+    } else {
+        Redirect::to("/").into_response()
+    }
 }
 
 pub async fn insert_securiy_headers(req: Request, next: Next) -> Response {
